3274c2573311b40fe1afebe7f1e062b39283cdfa
Rohit <roohitsingh17@gmail.com>
2023-03-19 22:33:08 +0530

    Automated sync:  M src/App.js ?? .dockerignore ?? Dockerfile.dev ?? sync.sh

---
+++ sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export 	PATH=$PATH:/usr/local/bin
+gstatus=`git status --porcelain`
+
+if [ ${#gstatus} -ne 0 ]
+then
+
+	git add --all
+	git commit -m "Automated sync: $gstatus"
+	git pull --rebase
+	git push
+
+fi
