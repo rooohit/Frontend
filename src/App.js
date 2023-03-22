85e0221ee7b9ccf7947c84ccd859732b0d64a04a
rooohit <41480879+rooohit@users.noreply.github.com>
2023-03-22 17:08:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js\\</code> and save to reload.
-         Version 1
+        Using Build ID
         </p>
         <a
           className="App-link"
