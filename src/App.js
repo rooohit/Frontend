47f6b22692cd331b86b9230c2c7946ea083ce459
Rohit <roohitsingh17@gmail.com>
2023-03-29 17:14:59 +0530

    Automated sync:  M src/App.js:This

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js\\</code> and save to reload.
-          Testing for continuous Integration and continuous Deployment. Trying for the second time.
+          Testing for continuous Integration as Deployment. Trying for the second time.
         </p>
         <a
           className="App-link"
