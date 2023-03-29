cfa8d9d5fa5ec749e1fd3fc54830342289147158
Rohit <roohitsingh17@gmail.com>
2023-03-29 16:49:23 +0530

    Automated sync:  M src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js\\</code> and save to reload.
-          Using Build from local PC
+          Using Build from local And on Google cloud platform
         </p>
         <a
           className="App-link"
